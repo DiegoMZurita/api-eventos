c5ecd441828dd8e8e3c5e8f4f7482fac8f5da83b
DiegoMZurita <thedaired@gmail.com>
2025-12-11 15:59:48 -0300

    feat: agregar DTOs, mapper con MapStruct y controller REST para listar y crear eventos

---
+++ src/main/java/com/gestion/eventos/api/mapper/EventMapper.java
@@ -0,0 +1,15 @@
+package com.gestion.eventos.api.mapper;
+
+import com.gestion.eventos.api.domain.Event;
+import com.gestion.eventos.api.dto.EventRequestDto;
+import com.gestion.eventos.api.dto.EventResponseDto;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface EventMapper {
+    List<EventResponseDto> toEventResponseDtoList(List<Event> events);
+    Event toEntity(EventRequestDto eventRequestDto);
+    EventResponseDto toResponseDto(Event event);
+}

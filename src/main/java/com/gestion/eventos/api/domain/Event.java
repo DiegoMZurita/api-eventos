585329062b6498cdd39c7d46fafc75cce856bd04
DiegoMZurita <thedaired@gmail.com>
2025-12-16 17:49:35 -0300

    feat: creación de entidades Category y Speaker y mejora de relaciones JPA

--- src/main/java/com/gestion/eventos/api/domain/Event.java
+++ src/main/java/com/gestion/eventos/api/domain/Event.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @Entity
@@ -13,7 +15,37 @@ public class Event {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private LocalDate date;
+
+    @Column(nullable = false)
     private String location;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(
+            name = "event_speakers",
+            joinColumns = @JoinColumn(name = "event_id"),
+            inverseJoinColumns = @JoinColumn(name = "speakers_id")
+    )
+    private Set<Speaker> speakers = new HashSet<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @ManyToMany(mappedBy = "attendedEvents", fetch = FetchType.LAZY)
+    private Set<User> attendedUsers = new HashSet<>();
+
+    public void addSpeaker(Speaker speaker){
+        this.speakers.add(speaker);
+        speaker.getEvents().add(this);
+    }
+
+    public void removeSpeaker(Speaker speaker){
+        this.speakers.remove(speaker);
+        speaker.getEvents().remove(this);
+    }
 }
